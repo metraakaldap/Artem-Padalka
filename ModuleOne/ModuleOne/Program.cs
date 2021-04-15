a288dc931025ebb8f04f0ea4474ac6d64b394c88
Artem Padalka <metraakladap228@gmail.com>
2021-04-15 22:36:05 +0300

    create a project

---
+++ ModuleOne/ModuleOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModuleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
