ef3298a7eaf9226efeb8b01cfbef71fa041227ec
Artem Padalka <metraakladap228@gmail.com>
2021-04-18 17:09:55 +0300

    added some new code

--- ModuleOne/ModuleOne/Program.cs
+++ ModuleOne/ModuleOne/Program.cs
@@ -15,7 +15,34 @@ namespace ModuleOne
         private static void Main(string[] args)
 #pragma warning restore SA1614 // Element parameter documentation should have text
         {
-            Console.WriteLine("HW");
+            int count1 = 0;
+            int count = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] vs = new int[n];
+            for (int i = 0; i < vs.Length; i++)
+            {
+                vs[i] = new Random().Next(1, 26);
+                if (vs[i] % 2 == 0)
+                {
+                    count++;
+                }
+
+                if (vs[i] % 2 != 0)
+                {
+                    count1++;
+                }
+            }
+
+            int[] evenArr = new int[count];
+            for (int i = 0; i < evenArr.Length; i++)
+            {
+                if (vs[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            int[] oddArr = new int[count1];
         }
     }
 }
