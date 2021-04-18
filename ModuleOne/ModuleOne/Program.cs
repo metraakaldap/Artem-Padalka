b75734685e39eedd98d74345bc122b416a3ce228
Artem Padalka <metraakladap228@gmail.com>
2021-04-18 16:30:41 +0300

    added stylecop

--- ModuleOne/ModuleOne/Program.cs
+++ ModuleOne/ModuleOne/Program.cs
@@ -2,11 +2,20 @@
 
 namespace ModuleOne
 {
-    class Program
+    /// <summary>
+    /// classOne.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+#pragma warning disable SA1614 // Element parameter documentation should have text
+        /// <summary>
+        /// main.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+#pragma warning restore SA1614 // Element parameter documentation should have text
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HW");
         }
     }
 }
